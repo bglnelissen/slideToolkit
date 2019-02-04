8d7d905cff9a913015d2069fe2dbe94984aa5d0d
swvanderlaan <s.w.vanderlaan@gmail.com>
2019-02-04 09:24:14 +0100

    Small edit to scripts; change name.

--- utilities/slideProcessor.py
+++ utilities/slideProcessor.py
@@ -11,22 +11,25 @@ NOWTIME = datetime.datetime.now()
 # evaluate arguments
 parser = argparse.ArgumentParser(
     prog='slideProcessor',
+    description='This script processes all images of a given stain from the BULK and puts them in a sequentially numbered series of folders.',
+    usage='slideCopy [-h/--help] -s/--stain STAIN -p/--path PATH',
     formatter_class=argparse.RawDescriptionHelpFormatter,
     epilog=textwrap.dedent("Copyright (c) 2018-2019 Sander W. van der Laan | s.w.vanderlaan-2@umcutrecht.nl; Tim G.M. van de Kerkhof | t.g.m.vandekerkhof@umcutrecht.nl."))
 parser.add_argument('-s','--stain', help='Give the name of the stain, e.g. CD34', required=True)
-parser.add_argument('-p','--path-to-stains', help='Give the full path to where the stain directory resides, e.g. /data/isi/d/dhl/ec/VirtualSlides/AE-SLIDES/', required=True)
+parser.add_argument('-p','--path', help='Give the full path to where the stain directory resides, e.g. /data/isi/d/dhl/ec/VirtualSlides/AE-SLIDES/', required=True)
 
 try:
     args = parser.parse_args()
-#     print("We are moving files for stain [" + args.stain + "] in [" args.path_to_stains + "].")
+  print("We are moving files for stain [" + args.stain + "] in [" + args.path + "].")
 
 except SystemExit:
-    print("Oh, computer says no! You must supply correct arguments when running a *** slideProcessor ***!")
+    print("\nOh, computer says no! You must supply correct arguments when running a *** slideProcessor ***!\n")
     parser.print_help()
+    exit()
 
 # globals
 STAIN = args.stain
-PATHS = args.path_to_stains + "/" + STAIN
+PATHS = args.path + "/" + STAIN
 BASE = PATHS + "/_"
 
 TIFnames = glob.glob(PATHS+"/*.TIF")
