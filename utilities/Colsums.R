67ceca2ee3747e5593c9d76b130869e84903115e
swvanderlaan <s.w.vanderlaan@gmail.com>
2019-07-02 21:06:15 +0200

    Added slideEMask

--- utilities/Colsums.R
+++ utilities/Colsums.R
@@ -1,6 +1,6 @@
 library(data.table)
 args = commandArgs(trailingOnly=TRUE)
-df = fread(paste0(args[1], "_Image.txt"))
+df = fread(paste0(args[1], "_", args[2]))
 VAL = sum(df$AreaOccupied_AreaOccupied_DAB_object_yellow) / sum(df$AreaOccupied_AreaOccupied_Tissue_object_green)
 if (is.na(VAL)) {VAL=0}
 cat( VAL )
